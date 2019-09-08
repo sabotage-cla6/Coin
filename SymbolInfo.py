837f9342881c81c0e9f53db4784a1ce9fb58b4b3
sabotage-cla6 <nines.gate1112345678999@gmail.com>
2019-09-08 15:49:56 +0900

    save mongoDB.

--- SymbolInfo.py
+++ SymbolInfo.py
@@ -1,20 +1,29 @@
 import requests
 import json
+from pymongo import MongoClient
 
 
-class CoinInfo:
+class SymbolInfo:
     ENDPOINT: str = 'https://api.coin.z.com/public'
-    TICKERPATH:str = '/v1/ticker?symbol={}'
+    TICKER_PATH: str = '/v1/ticker?symbol={}'
+
+    DB_SERVER: str = 'mongodb://localhost:27017/'
+    DB_NAME: str = 'COIN'
 
     def __init__(self, symbol: str):
         self.symbol = symbol
+        self.mongo_client = MongoClient(SymbolInfo.DB_SERVER)
+        self.db = self.mongo_client.COIN
 
     def get_ticker(self):
-        path = CoinInfo.TICKERPATH.format(self.symbol)
+        path = SymbolInfo.TICKER_PATH.format(self.symbol)
 
-        response = requests.get(CoinInfo.ENDPOINT + path)
+        response = requests.get(SymbolInfo.ENDPOINT + path)
         print(json.dumps(response.json(), indent=2))
 
+#        resdata: json = json.dumps(response.json(), indent=2)
+        self.db.Ticker.insert_one(response.json())
 
-symbol1 = CoinInfo('XRP_JPY')
+
+symbol1 = SymbolInfo('XRP_JPY')
 symbol1.get_ticker()
