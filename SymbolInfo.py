c6970045c918a388ebf8e2c8edd4e4bac10da977
sabotage-cla6 <nines.gate1112345678999@gmail.com>
2019-09-14 22:53:09 +0900

    don't let duplication data insert

--- SymbolInfo.py
+++ SymbolInfo.py
@@ -1,14 +1,22 @@
 from datetime import datetime
+from datetime import time
 from datetime import timedelta
 import requests
-import json
-from pymongo import MongoClient
+from bson import json_util
+
+import pymongo
 from enum import Enum
 import re
 import time
 
 
 class SymbolInfo:
+    _DB_SERVER: str = 'mongodb://localhost:27017/'
+    _DB_NAME: str = 'COIN'
+
+    _MONGO_CLIENT = pymongo.MongoClient(_DB_SERVER)
+    _DB = _MONGO_CLIENT.COIN
+
     class SYMBOLS(Enum):
         def __init__(self, gmo_name, dmm_name, dmm_req_name, gmo_url_path):
             self.gmo_name = gmo_name
@@ -22,31 +30,33 @@ class SymbolInfo:
     ENDPOINT: str = 'https://api.coin.z.com/public'
     TICKER_PATH: str = '/v1/ticker?symbol={}'
 
-    DB_SERVER: str = 'mongodb://localhost:27017/'
-    DB_NAME: str = 'COIN'
-
     def __init__(self, symbol: SYMBOLS):
         self.symbol = symbol
-        self.mongo_client = MongoClient(SymbolInfo.DB_SERVER)
-        self.db = self.mongo_client.COIN
 
     def save_chart(self):
         response = requests.get("https://bitcoin.dmm.com/trade_chart_rate_list/" + self.symbol.dmm_url_path)
         regex = re.compile(('<meta name="csrf-token" content="([^"]*)">'))
         token = regex.search(response.text).group(1)
-        print(token)
-        print(self.symbol.dmm_req_name)
 
         param = {"_token": token, "currency_name": self.symbol.dmm_req_name, "foot": "ONE_MIN", "type": "BID"}
         response = requests.post("https://bitcoin.dmm.com/api/get_realtime_chart", param)
-        # 一旦一時表に登録して、その結果を実テーブルに反映するように修正する
-        # とりあえずは重複ありで登録されるようにする
-        ins_data = response.json()["chart"][self.symbol.dmm_name]["ONE_MIN"]["BID"]
-        self.db[self.symbol.gmo_name].insert_many(ins_data)
-        print(ins_data)
+
+        temp_data = response.json()["chart"][self.symbol.dmm_name]["ONE_MIN"]["BID"]
+
+        # prevent insert duplicated data.
+        # use tmp collection. and select data need to insert.
+        max_date_registed = SymbolInfo._DB[self.symbol.name].find(projection={"d": 1}). \
+            sort("d", pymongo.DESCENDING).limit(1)[0]["d"]
+        tmp_collection_name: str = "tmp_{0}_{1}".format(self.symbol.name, time.time())
+        SymbolInfo._DB[tmp_collection_name].insert_many(temp_data)
+        find_temp_data = SymbolInfo._DB[tmp_collection_name].find({"d": {"$gt": max_date_registed}})
+        insert_data = [x for x in find_temp_data]
+        SymbolInfo._DB[self.symbol.name].insert_many(insert_data)
+        SymbolInfo._DB[tmp_collection_name].drop()
 
 
 if __name__ == '__main__':
+
     prev_time: datetime = datetime.now()
 
     while 1 == 1:
